e4b15021e05c2f8e7685d2196251c4d6a966aaac
yanyun <596310898@163.com>
2021-02-23 19:17:11 +0800

    添加环形进度条

--- src/views/progress/index.vue
+++ src/views/progress/index.vue
@@ -27,13 +27,13 @@ export default {
       val = Math.max(0, val);
       val = Math.min(100, val);
       if (val <= 50) {
-        bg2.style.transform = `rotate(${180 * val * 2 / 100}deg)`;
+        bg2.style.transform = `rotate(${(180 * val * 2) / 100}deg)`;
         bg2.style.borderColor = 'grey grey grey transparent';
         bg1.style.transform = 'rotate(0deg)';
       } else {
         bg2.style.transform = 'rotate(0deg)';
         bg2.style.borderColor = 'red red red transparent';
-        bg1.style.transform = `rotate(${180 * (val - 50) * 2 / 100}deg)`;
+        bg1.style.transform = `rotate(${(180 * (val - 50) * 2) / 100}deg)`;
       }
     },
   },
